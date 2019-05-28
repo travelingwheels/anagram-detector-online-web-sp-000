6438d9ba41658a1168fc3f45a4e8c31f5d7bb898
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-28 11:34:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+  @@words = []
+  
+  def initialize(words)
+    @@words = words
+  end
+  
+  def self.match(words)
+    words.split("").sort
+  end
+end
